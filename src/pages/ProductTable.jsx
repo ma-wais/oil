c6d1a781212546b20e3b95ee06ee44aefeabbf97
webagencyco <codewizardsuk@gmail.com>
2024-09-05 22:01:28 +0500

    inv change

--- src/pages/ProductTable.jsx
+++ src/pages/ProductTable.jsx
@@ -4,44 +4,36 @@ function ProductTable() {
   const [products, setProducts] = useState([]);
   const [productDetails, setProductDetails] = useState({
     billNo: "",
-    partyBillNo: "",
-    party: "",
-    ng: "",
-    product: "",
-    lotNo: "",
+    date: "",
+    customerName: "",
+    description: "",
+    quantity: "",
     weight: "",
-    cut: "",
-    filterWeight: "",
     unit: "",
     rate: "",
-    number: "",
-    bardana: "",
-    wages: "",
-    fare: "",
+    total: "",
+    wasool: "",
     netAmount: "",
-    purchaseExpense: "",
+    previousBalance: "",
+    grandTotal: "",
   });
 
   const addProduct = () => {
     setProducts([...products, productDetails]);
     setProductDetails({
       billNo: "",
-      partyBillNo: "",
-      party: "",
-      ng: "",
-      product: "",
-      lotNo: "",
+      date: "",
+      customerName: "",
+      description: "",
+      quantity: "",
       weight: "",
-      cut: "",
-      filterWeight: "",
       unit: "",
       rate: "",
-      number: "",
-      bardana: "",
-      wages: "",
-      fare: "",
+      total: "",
+      wasool: "",
       netAmount: "",
-      purchaseExpense: "",
+      previousBalance: "",
+      grandTotal: "",
     });
   };
 
@@ -51,16 +43,16 @@ function ProductTable() {
       <div className="grid grid-cols-4 gap-4 mb-4 text-right">
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Other History
+            Date
           </label>
           <input
-            type="text"
+            type="date"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.otherHistory}
+            value={productDetails.date}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                otherHistory: e.target.value,
+                date: e.target.value,
               })
             }
           />
@@ -83,133 +75,66 @@ function ProductTable() {
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Party
+            Customer Name
           </label>
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.party}
+            value={productDetails.customerName}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                party: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-2">
-            Cash Party Name
-          </label>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.partyBillNo}
-            onChange={(e) =>
-              setProductDetails({
-                ...productDetails,
-                partyBillNo: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-2">
-            Broker
-          </label>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.partyBillNo}
-            onChange={(e) =>
-              setProductDetails({
-                ...productDetails,
-                partyBillNo: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-2">
-            Adda
-          </label>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.partyBillNo}
-            onChange={(e) =>
-              setProductDetails({
-                ...productDetails,
-                partyBillNo: e.target.value,
+                customerName: e.target.value,
               })
             }
           />
         </div>
       </div>
 
-      <table className="w-full table-auto mb-4 text-right">
+      <table className="w-full table-auto my-10 text-right">
         <thead>
           <tr>
-            <th className="border px-4 py-2">Ng</th>
             <th className="border px-4 py-2">Product</th>
-            <th className="border px-4 py-2">Lot No.</th>
+            <th className="border px-4 py-2">Quantity</th>
             <th className="border px-4 py-2">Weight</th>
-            <th className="border px-4 py-2">Cut</th>
-            <th className="border px-4 py-2">Filter Weight</th>
-            <th className="border px-4 py-2">Unit</th>
             <th className="border px-4 py-2">Rate</th>
-            <th className="border px-4 py-2">Number</th>
-            <th className="border px-4 py-2">Bardana</th>
+            <th className="border px-4 py-2">Total</th>
           </tr>
         </thead>
         <tbody>
           {products.map((product, index) => (
             <tr key={index}>
-              <td className="border px-4 py-2">{product.ng}</td>
               <td className="border px-4 py-2">{product.product}</td>
-              <td className="border px-4 py-2">{product.lotNo}</td>
+              <td className="border px-4 py-2">{product.quantity}</td>
               <td className="border px-4 py-2">{product.weight}</td>
-              <td className="border px-4 py-2">{product.cut}</td>
-              <td className="border px-4 py-2">{product.filterWeight}</td>
-              <td className="border px-4 py-2">{product.unit}</td>
               <td className="border px-4 py-2">{product.rate}</td>
-              <td className="border px-4 py-2">{product.number}</td>
-              <td className="border px-4 py-2">{product.bardana}</td>
+              <td className="border px-4 py-2">{product.total}</td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      <div className="grid grid-cols-9 gap-4 mb-4 text-right">
-        {[
-          "Ng",
-          "Product",
-          "Lot No.",
-          "Weight",
-          "Cut",
-          "Filter Weight",
-          "Unit",
-          "Rate",
-          "Number",
-          "Bardana",
-        ].map((label, index) => (
-          <div key={index}>
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              {label}
-            </label>
-            <input
-              type="text"
-              className="w-full p-2 border border-gray-300 rounded-md"
-              value={productDetails[label.toLowerCase().replace(" ", "")]}
-              onChange={(e) =>
-                setProductDetails({
-                  ...productDetails,
-                  [label.toLowerCase().replace(" ", "")]: e.target.value,
-                })
-              }
-            />
-          </div>
-        ))}
+      <div className="grid grid-cols-5 gap-4 mb-4 text-right">
+        {["Product", "Quantity.", "Weight", "Rate", "Total"].map(
+          (label, index) => (
+            <div key={index}>
+              {/* <label className="block text-sm font-medium text-gray-700 mb-2">
+                {label}
+              </label> */}
+              <input
+                type="text"
+                className="w-full p-2 border border-gray-300 rounded-md"
+                value={productDetails[label.toLowerCase().replace(" ", "")]}
+                onChange={(e) =>
+                  setProductDetails({
+                    ...productDetails,
+                    [label.toLowerCase().replace(" ", "")]: e.target.value,
+                  })
+                }
+              />
+            </div>
+          )
+        )}
       </div>
 
       <button
@@ -219,40 +144,71 @@ function ProductTable() {
         Add Product
       </button>
 
-      <div className="grid grid-cols-2 gap-4 mb-4 text-right">
+      <div className="grid grid-cols-4 gap-4 mb-4 text-right">
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Wages
+            Wasool
           </label>
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.wages}
+            value={productDetails.wasool}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                wages: e.target.value,
+                wasool: e.target.value,
               })
             }
           />
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Bardana
+            Total
           </label>
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.bardana}
+            value={productDetails.netAmount}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                bardana: e.target.value,
+                netAmount: e.target.value,
               })
             }
           />
         </div>
-        
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Previous Balance
+          </label>
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded-md"
+            value={productDetails.previousBalance}
+            onChange={(e) =>
+              setProductDetails({
+                ...productDetails,
+                previousBalance: e.target.value,
+              })
+            }
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Grand Total
+          </label>
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded-md"
+            value={productDetails.grandTotal}
+            onChange={(e) =>
+              setProductDetails({
+                ...productDetails,
+                grandTotal: e.target.value,
+              })
+            }
+          />
+        </div>
       </div>
     </div>
   );
--- src/pages/Purchase.jsx
+++ src/pages/Purchase.jsx
@@ -1,71 +1,49 @@
 import { useState } from "react";
 
-function PurchaseInv() {
+function ProductTable() {
   const [products, setProducts] = useState([]);
   const [productDetails, setProductDetails] = useState({
     billNo: "",
-    partyBillNo: "",
-    party: "",
-    ng: "",
-    product: "",
+    date: "",
+    customerName: "",
+    description: "",
+    quantity: "",
     weight: "",
-    cut: "",
-    filterWeight: "",
     unit: "",
     rate: "",
-    number: "",
-    bardana: "",
-    wages: "",
-    fare: "",
+    total: "",
+    wasool: "",
     netAmount: "",
-    purchaseExpense: "",
+    previousBalance: "",
+    grandTotal: "",
   });
 
   const addProduct = () => {
     setProducts([...products, productDetails]);
     setProductDetails({
       billNo: "",
-      partyBillNo: "",
-      party: "",
-      ng: "",
-      product: "",
+      date: "",
+      customerName: "",
+      description: "",
+      quantity: "",
       weight: "",
-      cut: "",
-      filterWeight: "",
       unit: "",
       rate: "",
-      number: "",
-      bardana: "",
-      wages: "",
-      fare: "",
+      total: "",
+      wasool: "",
       netAmount: "",
-      purchaseExpense: "",
+      previousBalance: "",
+      grandTotal: "",
     });
   };
 
   return (
     <div className="p-6 bg-white shadow-md rounded-md rtl">
-      <h1 className="text-xl font-semibold mb-4">Purchase Item Table</h1>
+      <h2 className="text-xl font-semibold mb-4">Product Sale Table</h2>
       <div className="grid grid-cols-4 gap-4 mb-4 text-right">
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Other History
-          </label>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.otherHistory}
-            onChange={(e) =>
-              setProductDetails({
-                ...productDetails,
-                otherHistory: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-2">
-            Bill No
+            Invoice No
           </label>
           <input
             type="text"
@@ -81,100 +59,83 @@ function PurchaseInv() {
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Party Bill No
+            Date
           </label>
           <input
-            type="text"
+            type="date"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.partyBillNo}
+            value={productDetails.date}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                partyBillNo: e.target.value,
+                date: e.target.value,
               })
             }
           />
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Party
+            Party Name
           </label>
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.party}
+            value={productDetails.customerName}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                party: e.target.value,
+                customerName: e.target.value,
               })
             }
           />
         </div>
       </div>
 
-      <table className="w-full table-auto mb-4 text-right">
+      <table className="w-full table-auto my-10 text-right">
         <thead>
           <tr>
-            <th className="border px-4 py-2">Ng</th>
             <th className="border px-4 py-2">Product</th>
+            <th className="border px-4 py-2">Quantity</th>
             <th className="border px-4 py-2">Weight</th>
-            <th className="border px-4 py-2">Cut</th>
-            <th className="border px-4 py-2">Filter Weight</th>
-            <th className="border px-4 py-2">Unit</th>
             <th className="border px-4 py-2">Rate</th>
-            <th className="border px-4 py-2">Number</th>
-            <th className="border px-4 py-2">Bardana</th>
+            <th className="border px-4 py-2">Total</th>
           </tr>
         </thead>
         <tbody>
           {products.map((product, index) => (
             <tr key={index}>
-              <td className="border px-4 py-2">{product.ng}</td>
               <td className="border px-4 py-2">{product.product}</td>
+              <td className="border px-4 py-2">{product.quantity}</td>
               <td className="border px-4 py-2">{product.weight}</td>
-              <td className="border px-4 py-2">{product.cut}</td>
-              <td className="border px-4 py-2">{product.filterWeight}</td>
-              <td className="border px-4 py-2">{product.unit}</td>
               <td className="border px-4 py-2">{product.rate}</td>
-              <td className="border px-4 py-2">{product.number}</td>
-              <td className="border px-4 py-2">{product.bardana}</td>
+              <td className="border px-4 py-2">{product.total}</td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      <div className="grid grid-cols-9 gap-4 mb-4 text-right">
-        {[
-          "Ng",
-          "Product",
-          "Weight",
-          "Cut",
-          "Filter Weight",
-          "Unit",
-          "Rate",
-          "Number",
-          "Bardana",
-        ].map((label, index) => (
-          <div key={index}>
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              {label}
-            </label>
-            <input
-              type="text"
-              className="w-full p-2 border border-gray-300 rounded-md"
-              value={productDetails[label.toLowerCase().replace(" ", "")]}
-              onChange={(e) =>
-                setProductDetails({
-                  ...productDetails,
-                  [label.toLowerCase().replace(" ", "")]: e.target.value,
-                })
-              }
-            />
-          </div>
-        ))}
+      <div className="grid grid-cols-5 gap-4 mb-4 text-right">
+        {["Product", "Quantity.", "Weight", "Rate", "Total"].map(
+          (label, index) => (
+            <div key={index}>
+              <label className="block text-sm font-medium text-gray-700 mb-2">
+                {label}
+              </label>
+              <input
+                type="text"
+                className="w-full p-2 border border-gray-300 rounded-md"
+                value={productDetails[label.toLowerCase().replace(" ", "")]}
+                onChange={(e) =>
+                  setProductDetails({
+                    ...productDetails,
+                    [label.toLowerCase().replace(" ", "")]: e.target.value,
+                  })
+                }
+              />
+            </div>
+          )
+        )}
       </div>
-
       <button
         onClick={addProduct}
         className="bg-blue-500 text-white py-2 px-4 rounded-md"
@@ -182,58 +143,100 @@ function PurchaseInv() {
         Add Product
       </button>
 
-      <div className="grid grid-cols-2 gap-4 mb-4 text-right">
+      <h1 className="block text-2xl mt-10 font-medium text-gray-700 mb-2">
+        Details
+      </h1>
+      <div className="grid grid-cols-5 gap-4 mb-4 text-right">
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Wages
+            Car No.
           </label>
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.wages}
+            value={productDetails.carNo}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                wages: e.target.value,
+                carNo: e.target.value,
               })
             }
           />
         </div>
+
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Bardana
+            Car Fare
           </label>
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.bardana}
+            value={productDetails.carFare}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                bardana: e.target.value,
+                carFare: e.target.value,
               })
             }
           />
         </div>
+
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Fare
+            Previous Weight
           </label>
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.fare}
+            value={productDetails.previousWeight}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                fare: e.target.value,
+                previousWeight: e.target.value,
               })
             }
           />
         </div>
+
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Net Amount
+            New Weight
+          </label>
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded-md"
+            value={productDetails.newWeight}
+            onChange={(e) =>
+              setProductDetails({
+                ...productDetails,
+                newWeight: e.target.value,
+              })
+            }
+          />
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Nag
+          </label>
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded-md"
+            value={productDetails.nag}
+            onChange={(e) =>
+              setProductDetails({
+                ...productDetails,
+                nag: e.target.value,
+              })
+            }
+          />
+        </div>
+      </div>
+      
+      <div className="grid grid-cols-4 gap-4 mb-4 text-right">
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Total
           </label>
           <input
             type="text"
@@ -249,16 +252,32 @@ function PurchaseInv() {
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
-            Purchase Expense
+            Previous Balance
           </label>
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded-md"
-            value={productDetails.purchaseExpense}
+            value={productDetails.previousBalance}
             onChange={(e) =>
               setProductDetails({
                 ...productDetails,
-                purchaseExpense: e.target.value,
+                previousBalance: e.target.value,
+              })
+            }
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Grand Total
+          </label>
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded-md"
+            value={productDetails.grandTotal}
+            onChange={(e) =>
+              setProductDetails({
+                ...productDetails,
+                grandTotal: e.target.value,
               })
             }
           />
@@ -268,4 +287,4 @@ function PurchaseInv() {
   );
 }
 
-export default PurchaseInv;
+export default ProductTable;
